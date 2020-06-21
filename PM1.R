7ab1c7560a1a5bc14509151c84a74aa71a7c3e0b
siddu138 <50289248+siddu138@users.noreply.github.com>
2020-06-21 17:14:36 +0530

    Delete PM1.R

--- PM1.R
+++
@@ -1,9 +0,0 @@
-totalEmission <- aggregate(Emissions ~ year, NEIdata, sum)
-totalEmission
-barplot(
-  (totalEmission$Emissions)/10^6,
-  names.arg=totalEmission$year,
-  xlab="Year",
-  ylab="PM2.5 Emissions (10^6 Tons)",
-  main="Total PM2.5 Emissions From All US Sources"
-)
